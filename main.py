bd13300425033f2b1715449c3d16870319dd2cc0
Boaz <minseong.chu@cmes.kr>
2020-12-07 00:53:35 +0900

    201207 work done. Try Apply

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QGridLayout, QLabel, QTextEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
+from pptx import Presentation  # 라이브러리
+from pptx.util import Inches  # 사진, 표등을 그리기 위해
 
 
 class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.btnConvert = QPushButton("입력 내용을 PPT로 변환(&S)")
+        self.texteditLyrics = QTextEdit()
+        self.labelLyrics = QLabel("찬양 가사")
+        self.texteditTitle = QTextEdit()
+        self.labelTitle = QLabel("찬양 제목")
         self.initUI()
 
     def initUI(self):
@@ -15,20 +22,15 @@ class MyApp(QWidget):
         QToolTip.setFont(QFont('SansSerif', 10))
 
         """위젯 아이템"""
-        labelTitle = QLabel("찬양 제목")
-        texteditTitle = QTextEdit()
-        labelLyrics = QLabel("찬양 가사")
-        texteditLyrics = QTextEdit()
-        btnConvert = QPushButton("Save Contents to Presentation")
 
         """그리드 레이아웃 설정"""
         grid = QGridLayout()
         self.setLayout(grid)
-        grid.addWidget(labelTitle, 0, 0)
-        grid.addWidget(texteditTitle, 1, 0)
-        grid.addWidget(labelLyrics, 2, 0)
-        grid.addWidget(texteditLyrics, 3, 0)
-        grid.addWidget(btnConvert, 4, 0)
+        grid.addWidget(self.labelTitle, 0, 0)
+        grid.addWidget(self.texteditTitle, 1, 0)
+        grid.addWidget(self.labelLyrics, 2, 0)
+        grid.addWidget(self.texteditLyrics, 3, 0)
+        grid.addWidget(self.btnConvert, 4, 0)
 
         """Stylesheet"""
         self.setStyleSheet(
@@ -36,7 +38,7 @@ class MyApp(QWidget):
             "color : #CFD6C9;"
         )
 
-        texteditTitle.setStyleSheet(
+        self.texteditTitle.setStyleSheet(
             """
             QTextEdit{
                 min-height : 50px;
@@ -45,7 +47,7 @@ class MyApp(QWidget):
             """
         )
 
-        btnConvert.setStyleSheet(
+        self.btnConvert.setStyleSheet(
             """
             QPushButton{
                 min-height : 50px;
@@ -61,6 +63,34 @@ class MyApp(QWidget):
         self.move(200, 100)
         self.resize(1400, 800)
         self.show()
+
+        """이벤트 핸들러 등록"""
+        self.btnConvert.clicked.connect(self.btnConvertClicked)
+
+    def btnConvertClicked(self):
+        pptFile = Presentation("./template.pptx")
+
+        ccm_title = self.texteditTitle.toPlainText()
+        ccm_lyrics_raw = self.texteditLyrics.toPlainText().split("\n")
+        lyrics_length = len(ccm_lyrics_raw)
+        ccm_lyrics = []
+
+        for idx in range(0, lyrics_length):
+            if idx == lyrics_length - 1:
+                if idx % 2 == 0:
+                    ccm_lyrics.append(ccm_lyrics_raw[idx])
+                    break
+            if idx % 2 == 0:
+                ccm_lyrics.append(ccm_lyrics_raw[idx] + "\n" + ccm_lyrics_raw[idx+1])
+
+        for idx in range(0, len(ccm_lyrics)):
+            slide = pptFile.slides[idx]
+            title = slide.shapes[1]
+            title.text = ccm_title
+            lyrics = slide.shapes[0]
+            lyrics.text = ccm_lyrics[idx]
+
+        pptFile.save("./" + ccm_title + ".pptx")
 
 
 if __name__ == '__main__':
